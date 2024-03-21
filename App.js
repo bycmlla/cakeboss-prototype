80725e32d91fe1f552be4a5b5d367c37ed19371f
Camila <by.cmlla0107@gmail.com>
2024-03-20 22:24:40 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts, Montserrat_300Light, Montserrat_600SemiBold} from "@expo-google-fonts/montserrat";
+import { Home } from "./screens/Home/Home";
+import Logo from "./assets/images/icons/logo.png";
+
+const Stack = createNativeStackNavigator();
+
+const CustomHeader = ({ title }) => {
+  return (
+    <View style={styles.headerContainer}>
+      <Image source={Logo} style={styles.logo} />
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_300Light,
+    Montserrat_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: styles.header,
+          headerTitleAlign: "center",
+          headerTitle: ({ children }) => <CustomHeader title={children} />,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} options={{ title: "O que deseja fazer?" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  logo: {
+    width: 300,
+    height: 100,
+    resizeMode: "contain",
+    marginRight: 10,
+  },
+  header: {
+    backgroundColor: "#FEA500",
+  },
+  headerTitle: {
+    color: "white",
+    fontFamily: 'Montserrat_600SemiBold',
+    fontSize: 30,
+    marginTop: -15,
+    marginBottom: 10
   },
 });
---
+++ screens/Home/Home.js
@@ -0,0 +1,12 @@
+// ./screens/Home/Home.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to Home Screen!</Text>
+    </View>
+  );
+};
