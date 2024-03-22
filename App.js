8bc5e477c3e62c45130bc8568cf7e5dbf40afd54
Camila <by.cmlla0107@gmail.com>
2024-03-21 21:46:47 -0300

    finally

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import React from "react";
 import { StyleSheet, Image, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useFonts, Montserrat_300Light, Montserrat_600SemiBold} from "@expo-google-fonts/montserrat";
+import {
+  useFonts,
+  Montserrat_600SemiBold,
+} from "@expo-google-fonts/montserrat";
+import { StatusBar } from "expo-status-bar";
 import { Home } from "./screens/Home/Home";
+import Cadastrar from "./screens/Cadastrar/Cadastrar";
+import GerarEstoque from "./screens/GerarEstoque/GerarEstoque";
+import Precificar from "./screens/Precificar/Precificar";
 import Logo from "./assets/images/icons/logo.png";
-
+import Precificar2 from "./screens/Precificar2/Precificar2";
 const Stack = createNativeStackNavigator();
 
 const CustomHeader = ({ title }) => {
@@ -19,8 +26,7 @@ const CustomHeader = ({ title }) => {
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Montserrat_300Light,
-    Montserrat_600SemiBold
+    Montserrat_600SemiBold,
   });
 
   if (!fontsLoaded) {
@@ -29,14 +35,39 @@ export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar style="light" backgroundColor="#98680F" />
       <Stack.Navigator
         screenOptions={{
-          headerStyle: styles.header,
+          headerStyle: [styles.header, styles.shadow],
           headerTitleAlign: "center",
           headerTitle: ({ children }) => <CustomHeader title={children} />,
         }}
       >
-        <Stack.Screen name="Home" component={Home} options={{ title: "O que deseja fazer?" }} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "O que deseja fazer?" }}
+        />
+        <Stack.Screen
+          name="Cadastrar"
+          component={Cadastrar}
+          options={{ title: "Cadastrar Produto" }}
+        />
+        <Stack.Screen
+          name="Estoque"
+          component={GerarEstoque}
+          options={{ title: "Estoque de Produtos" }}
+        />
+        <Stack.Screen
+          name="Precificar"
+          component={Precificar}
+          options={{ title: "Precificação" }}
+        />
+        <Stack.Screen
+          name="Precificar2"
+          component={Precificar2}
+          options={{ title: "Precificação" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -58,9 +89,15 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     color: "white",
-    fontFamily: 'Montserrat_600SemiBold',
+    fontFamily: "Montserrat_600SemiBold",
     fontSize: 30,
-    marginTop: -15,
-    marginBottom: 10
+    marginTop: -12,
+    marginBottom: 20,
+  },
+  shadow: {
+    shadowColor: '#171717',
+    shadowOffset: {width: 5, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
